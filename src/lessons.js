935959faf20910827b14807f44ced4dc1c7fda5d
keisha <keishaowino712@gmail.com>
2023-11-12 22:02:18 +0000

    Add files via upload
    Files

---
+++ src/lessons.js
@@ -0,0 +1,84 @@
+export const lessonsData = [
+  {
+    id: 1,
+    subject: 'Mathematics',
+    location: 'Room A',
+    price: 30,
+    spaces: 5,
+    icon: 'https://images.squarespace-cdn.com/content/v1/53472e1fe4b039879a5867c0/1602515447418-Y2ITR0OCCDTPT934SK6R/math.png'
+  },
+  {
+    id: 2,
+    subject: 'Science',
+    location: 'Lab 101',
+    price: 25,
+    spaces: 5,
+    icon: 'https://cdn-icons-png.flaticon.com/512/1046/1046269.png'
+  },
+  {
+    id: 3,
+    subject: 'History',
+    location: 'Library',
+    price: 20,
+    spaces: 5,
+    icon: 'https://cdn-icons-png.flaticon.com/512/2234/2234770.png'
+  },
+  {
+    id: 4,
+    subject: 'Programming',
+    location: 'Computer Lab',
+    price: 40,
+    spaces: 5,
+    icon: 'https://cdn-icons-png.flaticon.com/512/2621/2621040.png'
+  },
+  {
+    id: 5,
+    subject: 'Art',
+    location: 'Art Studio',
+    price: 35,
+    spaces: 5,
+    icon: 'https://cdn-icons-png.flaticon.com/512/103/103461.png'
+  },
+  {
+    id: 6,
+    subject: 'Music',
+    location: 'Music Room',
+    price: 30,
+    spaces: 5,
+    icon: 'https://cdn-icons-png.flaticon.com/512/2907/2907197.png'
+  },
+  {
+    id: 7,
+    subject: 'Football',
+    location: 'School Field',
+    price: 15,
+    spaces: 5,
+    icon: 'https://cdn-icons-png.flaticon.com/512/1071/1071234.png'
+  },
+  {
+    id: 8,
+    subject: 'Chinese',
+    location: 'Room 117',
+    price: 35,
+    spaces: 5,
+    icon: 'https://cdn-icons-png.flaticon.com/512/3898/3898082.png'
+  },
+  {
+    id: 9,
+    subject: 'Cooking',
+    location: 'Food Tech Room',
+    price: 25,
+    spaces: 5,
+    icon: 'https://cdn-icons-png.flaticon.com/512/1027/1027128.png'
+  },
+  {
+    id: 10,
+    subject: 'Ballet',
+    location: 'Dance Studio',
+    price: 30,
+    spaces: 5,
+    icon: 'https://cdn-icons-png.flaticon.com/512/2251/2251941.png'
+  }
+];
+
+// export default lessons;
